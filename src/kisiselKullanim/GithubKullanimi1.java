934e5653083a7ed2e9d6fc1da6400e4389986e2f
ftuglu <fatih.tuglu@yahoo.com>
2023-06-01 14:42:19 -0400

    v.0.02

--- src/kisiselKullanim/GithubKullanimi1.java
+++ src/kisiselKullanim/GithubKullanimi1.java
@@ -1,7 +1,11 @@
 package kisiselKullanim;
 
+import java.sql.SQLOutput;
+
 public class GithubKullanimi1 {
     public static void main(String[] args) {
         System.out.println("hello world");
+
+        System.out.println("that s it");
     }
 }
